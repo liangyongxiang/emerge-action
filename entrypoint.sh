c6ba4f5ae68f94976c77d626c4cd01a944d7d703
Yongxiang Liang <tanekliang@gmail.com>
2022-02-04 13:44:20 +0800

    fixed live package and using local repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,25 +26,30 @@ echo "::endgroup::"
 echo "::group::overlay prepare"
 emerge-webrsync
 eselect news read all > /dev/null
-emerge app-eselect/eselect-repository dev-vcs/git
+emerge app-eselect/eselect-repository app-portage/gentoolkit dev-vcs/git
 echo "::endgroup::"
 
 
 echo "::group::overlay sync"
-if [ ! -z "${INPUT_OVERLAY_NAME}" ]; then
-    eselect repository add ${INPUT_OVERLAY_NAME} ${INPUT_OVERLAY_SYNC_TYPE} ${INPUT_OVERLAY_SYNC_URI}
-    emerge --sync ${INPUT_OVERLAY_NAME}
-fi
+repo_name=$(cat "${GITHUB_WORKSPACE}/profiles/repo_name")
+eselect repository add $repo_name git "file://${GITHUB_WORKSPACE}"
+emerge --sync $repo_name
 echo "::endgroup::"
 
 
 echo "::group::install"
+for pkg in ${INPUT_PACKAGES}; do
+    keywords=$(equery meta "$pkg::$repo_name" | grep 'Keywords:')
+    if [[ "$keywords" != *"amd64"* ]]; then
+        echo "$pkg::$repo_name **" >> /etc/portage/package.accept_keywords/$(basename $pkg)
+    fi
+done
 emerge ${INPUT_PACKAGES}
 result=$?
 echo "::endgroup::"
 
 
-echo "::group::install"
+echo "::group::post install"
 for file in $(ls "${INPUT_PORTAGE_TMPDIR}/portage/*/*/temp/build.log" 2>/dev/null); do
     echo "$file:"
     cat $file
